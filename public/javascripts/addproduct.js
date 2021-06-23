51a7989cc23f977ea31cab6c66807742de509286
CompanyDATV <companydatv412@gmail.com>
2021-06-23 23:01:22 +0700

    update by datv tonight

---
+++ public/javascripts/addproduct.js
@@ -0,0 +1,227 @@
+function readURl(input) {
+    if (input.file && input.file[0]) {
+        var reader = new FileReader;
+        reader.onload = function (e) {
+            $("#imgPreview").attr('src', e.target.result).width(100);
+        }
+        reader.readAsDataURL(input.file[0]);
+    }
+}
+$("#img").change(function () {
+    readURl(this)
+});
+
+var loadFile = function (event) {
+    var image = document.getElementById('output');
+    image.src = URL.createObjectURL(event.target.files[0]);
+};
+$("#output").addClass("img-post-show");
+$("#output").removeClass("img-2");
+
+function myA() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function myB() {
+    var x = document.getElementById("myDIV1");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function myC() {
+    var x = document.getElementById("myDIV-A");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function myfunctionA2() {
+    var x = document.getElementById("myDIV-A");
+    x.style.display = "none";
+}
+
+
+
+function myfunctionA1() {
+    var x = document.getElementById("myDIV1");
+    x.style.display = "none";
+}
+function myfunctionB1() {
+    var x = document.getElementById("myDIV");
+    x.style.display = "none";
+}
+
+function checkOut1() {
+    document.getElementById('khong').style.display = 'block';
+    document.getElementById('dongy').style.display = 'none';
+}
+function checkOut2() {
+    document.getElementById('khong').style.display = 'none';
+    document.getElementById('dongy').style.display = 'block';
+    document.getElementById('dongy').style.display = "flex";
+}
+
+
+$('#imageUpload').change(function () {
+    readImgUrlAndPreview(this);
+
+    function readImgUrlAndPreview(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#imagePreview').removeClass('hide').attr('src', e.target.result);
+                $('#btnfile').addClass('hidden')
+            }
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+});
+$('#imageUpload1').change(function () {
+    readImgUrlAndPreview(this);
+
+    function readImgUrlAndPreview(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#imagePreview1').removeClass('hide').attr('src', e.target.result);
+                $('#btnfile1').addClass('hidden');
+            }
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+});
+$('#imageUpload2').change(function () {
+    readImgUrlAndPreview(this);
+
+    function readImgUrlAndPreview(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#imagePreview2').removeClass('hide').attr('src', e.target.result);
+                $('#btnfile2').addClass('hidden');
+            }
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+});
+$('#imageUpload3').change(function () {
+    readImgUrlAndPreview(this);
+
+    function readImgUrlAndPreview(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#imagePreview3').removeClass('hide').attr('src', e.target.result);
+                $('#btnfile3').addClass('hidden');
+            }
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+});
+$('#imageUpload4').change(function () {
+    readImgUrlAndPreview(this);
+
+    function readImgUrlAndPreview(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#imagePreview4').removeClass('hide').attr('src', e.target.result);
+                $('#btnfile4').addClass('hidden');
+            }
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+});
+$('#imageUpload5').change(function () {
+    readImgUrlAndPreview(this);
+
+    function readImgUrlAndPreview(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#imagePreview5').removeClass('hide').attr('src', e.target.result);
+                $('#btnfile5').addClass('hidden');
+            }
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+});
+$('#imageUpload6').change(function () {
+    readImgUrlAndPreview(this);
+
+    function readImgUrlAndPreview(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#imagePreview6').removeClass('hide').attr('src', e.target.result);
+                $('#btnfile6').addClass('hidden');
+            }
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+});
+$('#imageUpload7').change(function () {
+    readImgUrlAndPreview(this);
+
+    function readImgUrlAndPreview(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#imagePreview7').removeClass('hide').attr('src', e.target.result);
+                $('#btnfile7').addClass('hidden');
+            }
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+});
+$('#imageUpload8').change(function () {
+    readImgUrlAndPreview(this);
+
+    function readImgUrlAndPreview(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#imagePreview8').removeClass('hide').attr('src', e.target.result);
+                $('#btnfile8').addClass('hidden');
+            }
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+});
+$('#imageUpload9').change(function () {
+    readImgUrlAndPreview(this);
+
+    function readImgUrlAndPreview(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#imagePreview9').removeClass('hide').attr('src', e.target.result);
+                $('#btnfile6').addClass('hidden');
+            }
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+});
+$('#imageUpload10').change(function () {
+    readImgUrlAndPreview(this);
+
+    function readImgUrlAndPreview(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#imagePreview10').removeClass('hide').attr('src', e.target.result);
+                $('#btnfile10').addClass('hidden');
+            }
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+});
